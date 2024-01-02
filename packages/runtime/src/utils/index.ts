80a683704e18b6e05a7d9eaba307138f7351e9f8
siriwatknp <siriwatkunaporn@gmail.com>
2024-01-02 17:44:56 +0700

    simplify css vars generation

--- packages/runtime/src/utils/index.ts
+++ packages/runtime/src/utils/index.ts
@@ -3,5 +3,4 @@ export {
   createTheme,
 } from '@mui/material/styles';
 export type { PluginCustomOptions } from './cssFnValueToVariable';
-export * from './generateCss';
 export * from './preprocessor';
